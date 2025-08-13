c92c72c01ef9d62e344a718dc29e51bbe8f50393
Bernardo Nassr Conte <bernardonc2007@gmail.com>
2025-08-13 10:16:02 -0300

    geometria plana

--- aplicacao/app.java
+++ aplicacao/app.java
@@ -1,5 +1,4 @@
 package aplicacao;
-
 public class app {
     
 }
--- classes/geometriaPlana.java
+++ classes/geometriaPlana.java
@@ -2,5 +2,8 @@ package classes;
 
 public class geometriaPlana {
 
-    
+    public void quadrado (double lado, double area){
+        area = lado * lado;
+        System.out.println("A área do quadrado é: " + area);
+    }
 }
